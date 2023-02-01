1284c188d764a711c51b447e98cdb8ba9f5fd99b
Zhiwu Mao <zwmao@outlook.com>
2023-02-01 21:28:11 +0800

    修复secret显示错误

--- createToken.js
+++ createToken.js
@@ -3,5 +3,5 @@ import { generateToken } from './Helper/generatorHelper.js'
 
 let { password, secret } = config
 
-console.log(`根据密码:${password}、密文:{secret}生成的token:`);
+console.log(`根据密码:${password}、密文:${secret} 最终生成的token如下:`);
 console.log(generateToken(password, secret));
