83735733ab0238f526ae94bf5b5306d0c9a761aa
creatorMao <zwmao@outlook.com>
2024-12-06 22:07:54 +0800

    🐞 fix: 请求参数兼容全小写

--- Helper/httpHelper.js
+++ Helper/httpHelper.js
@@ -1,18 +1,16 @@
-const getParam = (req, key) => {
+const getParam = (req, key = "") => {
   let queryRes = "";
   let bodyRes = "";
 
   if (req.query) {
-    queryRes = req.query[key]
+    queryRes = req.query[key] || req.query[key.toLowerCase];
   }
 
   if (req.body) {
-    bodyRes = req.body[key]
+    bodyRes = req.body[key] || req.query[key.toLowerCase];
   }
 
-  return queryRes || bodyRes || ""
-}
+  return queryRes || bodyRes || "";
+};
 
-export {
-  getParam
-}
+export { getParam };
